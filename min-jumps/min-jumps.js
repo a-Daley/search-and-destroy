d09726c5ecc45d80d40a9f0c0b7707a1eecc9e6d
April Daley <daley.april@gmail.com>
2019-01-28 15:48:37 -0500

    sudo coded min-jumps

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -3,6 +3,25 @@
 // Complete this algo
 const minJumps = arr => {
 
-};
+}
 
 module.exports = minJumps
+
+//? edge case: check if the array is defined
+
+//? easiest case: n = 1, minJumps = 1
+
+//?if (arr.length <= arr[0]) {minJumps = 1}
+
+//set a counter to 1
+
+//? for each number you're on....
+
+//? currentNum > the length of the rest of the array 
+//? then increment counter by one and return the counter
+
+//? else -- recurse --- then check which of the other numbers within your jump range is the largest and jump to that one
+
+//?switch to that max number
+
+//? increment the counter
