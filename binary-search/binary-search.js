20ef79b3a7a451ab1c66fd50d79dc23cab654dea
April Daley <daley.april@gmail.com>
2019-01-28 12:26:40 -0500

    progress on BST

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,8 +2,51 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
-};
+	class BinarySearchTree {
+		constructor(val) {
+			this.value = val
+			this.left = null
+			this.right = null
+		}
+
+		insert(val) {
+			let direction
+			if (val < this.value) {
+				direction = 'left'
+			} else if (val > this.value) {
+					direction = 'right'
+			}
+
+			if (this[direction] === null) {
+				this[direction] = new BinarySearchTree(val)
+			} else {
+				this[direction].insert(val)
+			}
+		}
+		
+		contains(val) {
+			if (val === this.value) return true;
+			const direction = val < this.value ? 'left' : 'right';
+			return this[direction] ? this[direction].contains(val) : false;
+			}
+
+		}
+		console.log('Testing: ', array)
+
+		const Result = new BinarySearchTree(array.pop())
+
+
+		array.forEach(num => {
+			console.log(Result)
+			if (num !== undefined) {
+				return Result.insert(num)
+			}
+		})
+
+		return Result.contains(target)
+	}
+
+
 
 /*
 	EXTRA CREDIT:
